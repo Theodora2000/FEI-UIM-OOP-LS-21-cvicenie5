909e0e27b8cc60417c294cf4fa4a037331730e0d
Theodora2000 <xsimont@stuba.sk>
2021-03-16 10:31:02 +0100

    instanceof with subclasses

--- src/main/java/sk/stuba/uim/fei/oop/Main.java
+++ src/main/java/sk/stuba/uim/fei/oop/Main.java
@@ -7,13 +7,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Animal> animals = List.of(new Bird(), new Bird(), new Dog());//nemodifikovatelny list
+        List<Animal> animals = List.of(new Bird(),new Parrot(), new Bird(), new Dog());//nemodifikovatelny list
         var animalsEditable = new ArrayList<>(animals);//tuto ho uz mozem modifikovat
 
         for (Animal animal : animals) {
             animal.makeSound();
             if (animal instanceof Bird) {
-                ((Bird) animal).repeat("Hello world");
+                ((Bird) animal).fly();
+            }
+            if(animal instanceof Parrot){
+                ((Parrot) animal).repeat("Hello world");
             }
         }
     }
