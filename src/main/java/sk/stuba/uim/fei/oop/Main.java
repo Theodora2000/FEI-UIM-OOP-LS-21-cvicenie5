3d41381644812307f22295ac310a6102a5a1f13d
Theodora2000 <xsimont@stuba.sk>
2021-03-16 10:49:43 +0100

    enumm

--- src/main/java/sk/stuba/uim/fei/oop/Main.java
+++ src/main/java/sk/stuba/uim/fei/oop/Main.java
@@ -7,19 +7,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Animal> animals = List.of(new Bird(),new Parrot(), new Bird(), new Dog());//nemodifikovatelny list
-        var animalsEditable = new ArrayList<>(animals);//tuto ho uz mozem modifikovat
+        checkServer(SvetoveStrany.fromString("SEVER"));
+        checkServer(SvetoveStrany.fromString("sever"));
+    }
 
-        for (Animal animal : animals) {
-            animal.makeSound();
-            if (animal instanceof Bird) {
-                ((Bird) animal).fly();
+    static void checkServer(SvetoveStrany s){
+
+            if(s.jeSever){
+                System.out.println("rovne");
             }
-            if(animal instanceof Parrot){
-                ((Parrot) animal).repeat("Hello world");
+            else{
+                System.out.println("nerovne");
             }
-            System.out.println("----");
-        }
+
     }
 }
 
